c8ce46721556efc78acae0899e6b55e02ae7dbc7
Ashrafur Rahman Minhaj <ashraf_minhaj@yahoo.com>
2022-10-07 15:56:03 +0600

    ret detected text as response, updated init script

--- scripts/init.sh
+++ scripts/init.sh
@@ -5,7 +5,8 @@ echo "arg: $1"
 if [[ "$1" == "dev" || "$1" == "stage" || "$1" == "prod" ]]; 
     then
         echo "environement: $1"
-        terraform init -input=false -migrate-state -backend-config=backend.$1.conf -var-file=terraform.$1.tfvars -lock=false
+        # terraform init -input=false -migrate-state -backend-config=backend.$1.conf -var-file=terraform.$1.tfvars -lock=false
+        terraform init -input=false -backend-config=backend.$1.conf -var-file=terraform.$1.tfvars -lock=false
     else
         echo "Wrong Argument"
         echo "Pass 'dev', 'stage' or 'prod' only."
