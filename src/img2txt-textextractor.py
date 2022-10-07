5fbee63dd60542b74367b88761ced24dcc250c08
Ashrafur Rahman Minhaj <ashraf_minhaj@yahoo.com>
2022-10-07 15:52:33 +0600

    Get all texts in response

--- src/img2txt-textextractor.py
+++ src/img2txt-textextractor.py
@@ -59,12 +59,6 @@ def handler(event, context):
         for text in textDetections:
                 # print ('Detected text:' + text['DetectedText'])
                 texts +=  text['DetectedText'] + ' '
-                # print ('Confidence: ' + "{:.2f}".format(text['Confidence']) + "%")
-                # print ('Id: {}'.format(text['Id']))
-                # if 'ParentId' in text:
-                #     print ('Parent Id: {}'.format(text['ParentId']))
-                # print ('Type:' + text['Type'])
-                # print()
 
     logger.info("Detected text: ")
     logger.info(texts)
